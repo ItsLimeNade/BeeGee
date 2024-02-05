7f2d5159cddaebb120e99b2b0ce9a74b255b7514
LimeNade <alexandre.zarkua@gmail.com>
2024-02-05 23:29:28 +0100

    DaisyUI + Tailwind

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,31 +2,65 @@
     import { enhance } from "$app/forms";
 
     export let form;
-    let bloodGlucoseValue: number;
+    let bloodGlucose: number;
+    let trend: string
+    let unit: string
+    let status: string
+    let success: boolean = false;
 
-    const handleGlucoseReading = async () => {
-        bloodGlucoseValue = Number(form?.mesurement?.value)
+    const handleLogin = () => {
+        success = form?.success ? form?.success : false;
+    }
+
+    const handleGlucoseReading = () => {
+        if (!form?.mesurement) return
+        bloodGlucose = form.mesurement?.value
+        trend = form.trend
+        unit = form.mesurement.unit
+        status = form.status
     }
 </script>
 
-<form action="?/login" method="POST" use:enhance>
-	<label>
-		Email
-		<input name="email" type="email">
-	</label>
-	<label>
-		Password
-		<input name="password" type="password">
-	</label>
-	<button>Log in</button>
-</form>
-
-{#if bloodGlucoseValue}
-    <p>Your bg is at {bloodGlucoseValue} mg/dl.</p>
+{#if !success}
+    <div class="flex justify-center items-center h-screen">
+        <div class="card w-96 bg-base-100 shadow-xl ">
+            <div class="card-body">
+                <form action="?/login" method="POST" use:enhance class="pt-1">
+                    <div class="flex flex-col max-w-xs">
+                        <label class="flex flex-col max-w-xs">
+                            Email :
+                            <input name="email" type="email" class="input input-bordered w-auto max-w-xs ">
+                        </label>
+                        <label class="flex flex-col max-w-xs">
+                            Password :
+                            <input name="password" type="password" class="input input-bordered w-auto max-w-xs">
+                        </label>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary mt-2" on:click={handleLogin}>Log in</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 {:else}
-    <p>No bg readings yet.</p>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <div class="stats shadow">
+            <div class="stat">
+                {#if bloodGlucose}
+                    <div class="stat-title">Bg {status}</div>
+                    <div class="stat-value">{bloodGlucose} {unit} {trend}</div>
+                    <div class="stat-desc">Hello there!</div>
+                {:else}
+                    <div class="stat-title">No data</div>
+                    <div class="stat-value">No data</div>
+                    <div class="stat-desc">No data</div>
+                {/if}
+            </div>
+        </div>
+        <form action="?/getBloodGlucoseReading" method="POST" use:enhance>
+            <button on:click={handleGlucoseReading} class="btn btn-primary mt-2">Get Reading</button>
+        </form>
+    </div>
 {/if}
 
-<form action="?/getBloodGlucoseReading" method="POST" use:enhance>
-	<button on:click={handleGlucoseReading}>Get Reading</button>
-</form>
